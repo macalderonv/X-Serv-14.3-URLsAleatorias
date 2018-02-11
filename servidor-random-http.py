6300d0c689584cf08b2414e982e7a35da42cb900
macalderonv <calderonv94@gmail.com>
2018-02-11 21:55:49 +0100

    v1 servidor-random-http.py

---
+++ servidor-random-http.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import socket
+import random
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mySocket.bind((socket.gethostname(), 1234))
+mySocket.listen(5)
+
+try:
+    while True:
+        numero = random.randint(1, 1000000)
+        url = "http://localhost:1234/" + str(numero)
+
+        print('Waiting for connections')
+        (recvSocket, address) = mySocket.accept()
+
+        print('Request received:')
+        print(recvSocket.recv(2048))
+
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
+        b"<html><body><h1>Hello!!</h1></body></html>" +
+        bytes('<a href=' + url + '>Dame otra</a>\r\n', 'utf-8') +
+        b"\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    print("Closing binded socket")
+    mySocket.close()
