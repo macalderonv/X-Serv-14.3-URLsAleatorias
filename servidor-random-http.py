37e02a2c48765209bc77ec4e9aa8c5ac6918bd02
macalderonv <macv@alpha23.aulas.gsyc.urjc.es>
2018-02-12 16:11:45 +0100

    v3 servidor-random-http.p

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -19,11 +19,11 @@ try:
         print('Request received:')
         print(recvSocket.recv(2048))
 
-        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                        b"<html><body><h1>Hello!!</h1>" + 
-                        bytes('<a href=' + url + '>Dame otra</a>\r\n', 'utf-8') +
-                        b"</p>" +
-                        b"</body></html>" +
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n"+
+                        b"<html><body><h1>Hello!!</h1>"+ 
+                        bytes('<a href=' + url + '>Dame otra</a>\r\n', 'utf-8')+
+                        b"</p>"+
+                        b"</body></html>"+
                         b"\r\n")
         recvSocket.close()
         
